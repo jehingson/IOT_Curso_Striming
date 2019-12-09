faaab0548206b068ce6a8c6cc190b21988935143
Jehingson <jehingson25@gmail.com>
2019-12-09 00:46:14 -0430

    Probando la red wifi

--- IoT-Arduino/IoT-Arduino.ino
+++ IoT-Arduino/IoT-Arduino.ino
@@ -8,34 +8,53 @@ WiFiMulti wifiMulti;
 ESP8266WiFiMulti wifiMulti;
 # endif
 
-int ledIndicador = 2;
+int LedIndicador = 2;
+int Led = 14;
+int Boton = 0;
+boolean EstadoBoton = false;
 
 void setup() {
   Serial.begin(115200);
   delay(10);
   Serial.println("Iniciando Sistema");
-  pinMode(ledIndicador, OUTPUT);
+  pinMode(LedIndicador, OUTPUT);
+  pinMode(led, OUTPUT);
+  pinMode(Boton, INPUT);
   wifiMulti.addAP("edward", "lokysan777");
   wifiMulti.addAP("edward", "Lokysan777");
+  Conectado();
 }
   
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(wifiMulti.run() != WL_CONNECTED){
   Conectado();
-
+  }
+  ActualizarLed();
+  Actualizarboton();
 }
+void ActualizarLed(){
+   if(EstadoBoton){
+    digitalWrite(Led, 1);
+    }else{
+      digitalWrite(Led, 0);
+      }
+}
+
+void ActualizarBoton(){
+  if(digitalRead(Boton) == 1){
+    EstadoBoton = !EstadoBoton;
+    delay(500);
+    }
+}
+
 void Conectando(){
   if (wifiMulti.run() != WL_CONNECTED) {
-    digitalWrite(ledIndicador, 1);
+    digitalWrite(LedIndicador, 0);
     delay(2000);
-    digitalWrite(ledIndicador, 0);
+    digitalWrite(LedIndicador, 1);
     delay(2000);
   } else {
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-        
-  } 
+    
+    Serial.println("conectado wifi");    
   }
-  
+}

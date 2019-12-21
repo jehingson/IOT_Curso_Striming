0d2aa5603f90e492e5b396fc547dd043828d8afe
Jehingson <jehingson25@gmail.com>
2019-12-21 17:15:37 -0430

    Conectado MQTT paguina web

---
+++ skeetch.js
@@ -0,0 +1,69 @@
+let BotonActivar;
+let BotonApagar;
+let EstadoFondo = false;
+
+let BrokerMQTT = 'broker.shiftr.io';
+let PuertoMQTT = 80;
+let ClienteIDMQTT = "Pagina-" + Math.floor(Math.random()*1000);
+let UsuarioMQTT = "d09f357f";
+let ContrasenaMQTT = "2bbb49cb0c48832c";
+
+client = new Paho.MQTT.Client(BrokerMQTT, PuertoMQTT, ClienteIDMQTT);
+
+client.onConnectionLost = MQTTPerder;
+client.onMessageArrived = MQTTMensaje;
+
+
+client.connect({
+  onSuccess: CuandoConectadoMQTT,
+  userName: UsuarioMQTT,
+  password: ContrasenaMQTT
+});
+
+function MQTTPerder(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("MQTT Perdio coneccion Error:" + responseObject.errorMessage);
+  }
+}
+
+function MQTTMensaje(message) {
+  console.log("Mensaje recibido:" + message.payloadString);
+  let Mensaje = message.payloadString;
+  if(Mensaje == '1'){
+  	EstadoFondo = true;
+  	console.log("Encendiendo Fondo");
+  }else{
+    EstadoFondo = false;
+}
+}
+
+function CuandoConectadoMQTT() {
+  console.log("MQTT Conectado");
+   client.subscribe("/Jehingson/Boton");
+}
+
+
+
+function setup() {
+  createCanvas(200, 200);
+  BotonActivar = createButton('Activar Led');
+  BotonApagar = createButton('Apagar Led');
+  BotonActivar.mousePressed(ActivarLed);
+  BotonApagar.mousePressed(ApagarLed);
+  
+}
+function ApagarLed(){
+	console.log("Apagar led");
+}
+
+function ActivarLed(){
+	console.log(" Activar led ");
+}
+
+function draw(){
+	if(EstadoFondo){
+		background(0);
+	}else{
+		background(255);
+	}
+}
